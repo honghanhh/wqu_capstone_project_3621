a60ffc0217e42b6208a3dac2ad58bdbee85e5d10
Hồng Hạnh <hanh.usth@gmail.com>
2023-09-03 14:54:27 +0200

    remove wrong command

--- run.sh
+++ run.sh
@@ -7,6 +7,5 @@ python regime_switch_plot.py --data_version 'train_data.csv'
 python regime_switch_plot.py --data_version 'validation_data.csv'
 python regime_switch_plot.py --data_version 'test_data.csv'
 python bayesian.py  --train_data '../data/discretized_data/train_data.csv' --val_data '../data/discretized_data/validation_data.csv' --test_data '../data/discretized_data/test_data.csv' 
-# python bayesian.py  --train_data '../data/hmm_data/train_data.csv' --val_data '../data/hmm_data/validation_data.csv' --test_data '../data/hmm_data/test_data.csv' 
 python markov.py  --cleaned_data '../data/cleaned_data/test_data.csv' --hmm_data '../data/discretized_data/test_data.csv' 
 python eval.py 
